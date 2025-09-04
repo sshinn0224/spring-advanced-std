fbf7a238b2b817b3ce7b049f10ccf885d993f698
Jade-jaeho.shin <jaeho.shin@lulumedic.com>
2025-09-04 15:52:15 +0900

    chapter - ~ 28

---
+++ src/main/java/com/lulumedic/advance/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,40 @@
+package com.lulumedic.advance.app.v4;
+
+import com.lulumedic.advance.trace.logtrace.LogTrace;
+import com.lulumedic.advance.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+
+            @Override
+            protected Void call() {
+                // 저장로직
+                if(itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
