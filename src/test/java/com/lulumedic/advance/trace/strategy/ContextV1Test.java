fb9fb292bf0ffc81d50c61b103ad2f43c376d0b5
sshinn0224 <sshinn0224@gmail.com>
2025-09-10 07:35:37 +0900

    chapter - ~30

--- src/test/java/com/lulumedic/advance/trace/strategy/ContextV1Test.java
+++ src/test/java/com/lulumedic/advance/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package com.lulumedic.advance.trace.strategy;
 
+import com.lulumedic.advance.trace.strategy.code.strategy.ContextV1;
+import com.lulumedic.advance.trace.strategy.code.strategy.StrategyLogic1;
+import com.lulumedic.advance.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -33,4 +36,19 @@ public class ContextV1Test {
 
         log.info("resultTime = {}", resultTime);
     }
+
+    /**
+     * 전략 패턴 사용
+     */
+    @Test
+    void strategyV1Test() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
 }
---
+++ src/test/java/com/lulumedic/advance/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,6 @@
+package com.lulumedic.advance.trace.strategy.code.strategy;
+
+public interface Strategy {
+    void call();
+}
+
